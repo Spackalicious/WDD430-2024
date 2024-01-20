c78e809833d2de1047d3fcaef8c1604de8272397
Julie Spackman <spack84@byui.edu>
2024-01-20 14:00:25 -0700

    Wk 2 CMS App Updates

--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -1,5 +1,4 @@
 export class Contact {
-    // constructor(public id: number, public email: string, public phone: string, public imageUrl: string, public group: Array) {
     constructor(
         public id: string
         , public name: string
